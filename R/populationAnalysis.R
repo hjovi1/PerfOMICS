89ccef1fe29dc601823225b5a213fa16516acc43
hjovi1 <helena.jjovic@gmail.com>
2022-12-03 20:28:46 -0500

    population plots done

--- R/populationAnalysis.R
+++ R/populationAnalysis.R
@@ -1,4 +1,4 @@
-plots <-function(file){
+scatterPlot <-function(file){
   # read excel file with user provided genetic profile
   data <- read_excel(file)
   x <- data$Score
@@ -10,14 +10,20 @@ plots <-function(file){
        pch = 19, frame = FALSE)
   abline(lm(y ~ x, data = mtcars), col = "blue")
   return(p)
-
-  freplot <- hist(data, main = "Frequency of Scores", # Title
-                  sub = "Based on 23-polymorphisms", # Subtitle
-                  xlab = "Genetic Score",           # X-axis label
-                  ylab = "Frequency",
-                  color = 'blue')           # Y-axis label
-
-  #return(freplot)
 }
+
+#' @import readr
+frequencyPlot <- function(file){
+  library("readxl")
+  data <- read_excel(file)
+  f <- hist(data, main = "Frequency of Scores", # Title
+                  sub = "Based on 23-polymorphisms", # Subtitle
+                  xlab = "Genetic Score", # X-axis label
+                  ylab = "Frequency", # Y-axis label
+                  color = 'blue')
+  return(f)
+}
+
+#return(freplot)
 
 #[END] Written by Helena Jovic
